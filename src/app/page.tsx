df55d879053197db4e1a5c4ecda3200db03dbb22
Hammad Hussain <hammadhussain920@gmail.com>
2025-05-04 16:41:04 +0500

    "btn"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import { useState } from "react";
 
 export default function Home() {
   const [showIframe, setShowIframe] = useState(false);
-
+  const handleClick = () =>{
+    window.open("https://www.croxyproxy.com/", "_blank");
+  }
   return (
     <div className="min-dvw min-h-dvh flex justify-center items-center">
       <button onClick={
         () => {setShowIframe(true)
-        }
+        handleClick()}
         } className="bg-lime-600 text-white font-bold font-sans px-3 py-2 hover:bg-lime-700 cursor-pointer">Visit!</button>
 
 
