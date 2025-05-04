1ca2df1699f96f4e12436f6d11476a9f29d01d7e
Hammad Hussain <hammadhussain920@gmail.com>
2025-05-04 16:31:30 +0500

    "iframe"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,24 @@
 "use client"
 
-export default function Home() {
+import { useState } from "react";
 
-  const handleClick = async () =>{
-    window.open("/api/visit", "_blank");
-    const sendRequest = await fetch("/api/visit" , {
-      method : "GET"
-    });
-    const text = await sendRequest.text();
-    console.log(text);
-  }
+export default function Home() {
+  const [showIframe, setShowIframe] = useState(false);
 
   return (
     <div className="min-dvw min-h-dvh flex justify-center items-center">
-      <button onClick={handleClick} className="bg-lime-600 text-white font-bold font-sans px-3 py-2 hover:bg-lime-700 cursor-pointer">Visit!</button>
+      <button onClick={
+        () => {setShowIframe(true)
+        }
+        } className="bg-lime-600 text-white font-bold font-sans px-3 py-2 hover:bg-lime-700 cursor-pointer">Visit!</button>
+
+
+      {showIframe && (
+        <iframe
+          src="/api/visit"
+          className="w-full h-[90vh] border mt-4"
+        />
+      )}
     </div>
   )
 }
